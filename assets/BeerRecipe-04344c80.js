d437adb483a10726ab91adb44cd271fb5f6c4e4e
i-havr <i.havrylov@gmail.com>
2023-07-15 15:14:21 +0300

    Updates

---
+++ assets/BeerRecipe-04344c80.js
@@ -0,0 +1,1 @@
+import{u as s,j as e}from"./index-0b3835c7.js";function o(){const{beerId:r}=s();return e.jsx("div",{children:e.jsx("p",{children:`Hello, I'm page of #${r} recipe`})})}export{o as default};
